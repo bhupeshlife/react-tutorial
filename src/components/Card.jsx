981607ee571e2b41f4f7101b34e9d82ae37ad19c
Bhupesh Rahangdale <bhupeshrahangdale199@.com>
2025-05-23 13:40:59 +0530

    Refactor Card component to enhance prop logging and improve code comments for clarity

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 
+// 1. We use destructuring to pull out the props we need:
+//    userDetail is an object, myObject could be any data, myArray is an array.
 function Card({ userDetail, myObject, myArray }) {
-  console.log("userName", userDetail);
-  console.log("myObject", myObject);
-  console.log("myArray", myArray);
-  //   In above Card function, { userName, myObject, myArray } is an object that contains all the properties passed to the component
-  //   In react, we can also use destructuring to get the properties of the object
-  //   function Card(props) {return ()}
-  //   In above Card function, props is an object that contains all the properties passed to the component
-  //   console.log("props", props);
+  // 2. You can log props to see what’s inside them:
+  console.log("userDetail:", userDetail);
+  console.log("myObject:", myObject);
+  console.log("myArray:", myArray);
 
   return (
-    <div className="w-60 flex flex-col rounded-xl bg-gray-300 min-h-[19rem] ">
+    // 3. A container div for the card, with Tailwind CSS classes for styling
+    <div className="w-60 flex flex-col rounded-xl bg-gray-300 min-h-[19rem]">
+      
+      {/* 4. Image section */}
       <div>
         <img
           src="https://cdn.vox-cdn.com/thumbor/ZkmdkuJUTLgJh96_FWQ5zweGGxo=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg"
-          alt="test"
+          alt="Sample NFT"
           className="object-cover object-center rounded-t-xl"
         />
       </div>
+
+      {/* 5. Text section */}
       <div className="flex flex-col py-3 px-3 pb-10">
-        <div className="flex justify-between ">
-          <h1 className="font-bold  ">{userDetail.name}</h1>
-          
-        </div>
-        <div className="flex  justify-between">
-          <p>{userDetail.age}</p>
-          <p>{userDetail.address.city}</p>
+        {/* 5a. Name */}
+        <h1 className="font-bold">{userDetail.name}</h1>
+        
+        {/* 5b. Age and City side by side */}
+        <div className="flex justify-between">
+          <p>Age: {userDetail.age}</p>
+          <p>City: {userDetail.address.city}</p>
         </div>
       </div>
     </div>
