8a6cf01805e888538f2d70abb9b80024c0b473f0
Bhupesh Rahangdale <bhupeshrahangdale199@.com>
2025-05-23 13:38:20 +0530

    Refactor HomePage component and improve code comments for clarity

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
+// 1. Always import React when writing JSX
 import React from "react";
+// 2. Import any child components you plan to use
 import Counter from "../components/Counter";
 import Card from "../components/Card";
 
 function HomePage() {
-  let userDetail = {
+  // 3. Prepare any data you want to pass into your components as props
+  const userDetail = {
     name: "Bhupesh",
     age: 23,
     address: {
@@ -11,27 +14,29 @@ function HomePage() {
     },
   };
 
-  let myArray = [1, 2, 3, 4, 5];
+  const myArray = [1, 2, 3, 4, 5];
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: "100vh",
-        gap: "20px",
-        padding: "20px",
-      }}
-    >
+    // 4. Return your page layout. Here we use Tailwind CSS classes
+    <div className="flex flex-col items-center justify-center min-h-screen gap-5 p-5">
+      
+      {/* 5. Show a simple counter */}
       <Counter />
-      <Card userName="Bhupesh" userDetail={userDetail} myArray={myArray} />
-      {/* userName=Bhupesh is a props which i passed to <Card/> component */}
-      {/* myObject={myObject} is a props which i passed to <Card/> component */}
-      {/* myArray={myArray} is a props which i passed to <Card/> component */}
-      {/* <Card/> is component that is imported from (import Card from "./components/card";) */}
+
+      {/*
+        6. Render the Card component and give it three props:
+           - userName: a simple string
+           - userDetail: the object we made above
+           - myArray: the number array we made above
+      */}
+      <Card
+        userName="Bhupesh"
+        userDetail={userDetail}
+        myArray={myArray}
+      />
     </div>
   );
 }
 
+// 7. Export HomePage so other files (like App.jsx) can import and display it
 export default HomePage;
