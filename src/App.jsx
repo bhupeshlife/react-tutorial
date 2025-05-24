58fc78dacee223affe6296bd22a5aeac656ea288
Bhupesh Rahangdale <bhupeshrahangdale199@.com>
2025-05-24 14:58:32 +0530

    Refactor App component to implement routing and improve structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-// 1. Import the HomePage component from the pages folder
-import HomePage from "./pages/HomePage";
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Suspense } from "react";
+import { routes, NotFound } from "./routes/Routes";
+import Loader from "./components/Loader";
 // 2. Import your global CSS so its styles apply across the app
 import "./App.css";
 
 // 3. Define the App component, which is the root of your React app
 function App() {
   return (
-    <>
-      {/* 4. Render the HomePage component inside App */}
-      <HomePage />
-    </>
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          {routes.map(({ path, component: Component }, idx) => (
+            <Route key={idx} path={path} element={(<Component />)} />
+          ))}
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
